32b58cb14007d4615527e255fadb63ea576f65aa
0xKhdr <0xkhdr@gmail.com>
2025-04-20 22:25:19 +0200

    First commit

---
+++ src/Providers/ServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Frontier\Repositories\Providers;
+
+use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
+
+class ServiceProvider extends IlluminateServiceProvider
+{
+    public function register(): void {}
+
+    public function boot(): void {}
+}
